81bc0229287a5059fc126249a1183f39098c68f1
hksix <mh.haseeb@gmail.com>
2017-08-29 15:18:50 -0400

    Customizing app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Readout from './Readout.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the surface of the sun</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Current temperature at surface level: 
+          <Readout temperature={1000000} unit='f' />
         </p>
       </div>
     );
